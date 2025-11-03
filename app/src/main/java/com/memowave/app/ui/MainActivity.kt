225762c43f19885fed90c04dcbc1936340950dfd
Nikita <neckitionov@gmail.com>
2025-11-04 00:34:18 +0300

    fix: fix background color and paddings in MainActivity

--- app/src/main/java/com/memowave/app/ui/MainActivity.kt
+++ app/src/main/java/com/memowave/app/ui/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,7 +16,6 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import com.memowave.app.ui.navigation.NavGraph
 import com.memowave.app.ui.theme.MemowaveTheme
-import com.memowave.app.ui.theme.backgroundLight
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,8 +33,7 @@ fun Memowave() {
 
     MemowaveTheme {
         Surface(
-            modifier = Modifier.fillMaxSize().padding(horizontal = 8.dp),
-            color = backgroundLight
+            modifier = Modifier.fillMaxSize().background(color = MaterialTheme.colorScheme.background).padding(horizontal = 8.dp)
         ) {
             NavGraph(navController)
         }
